255149ad4235bad69be56c8c939dcc1fcd977d5b
Ravindra Kumar <ravidsrk@gmail.com>
2016-08-07 14:41:48 +0530

    Fixed renaming .gitignore file

--- archi.js
+++ archi.js
@@ -4,51 +4,55 @@ const renamer = require('renamer');
 const rimraf = require('rimraf');
 const async = require('async');
 const path = require('path');
+const mv = require('mv');
 const mkdirp = require('mkdirp');
 const Git = require("nodegit");
 const replace = require("replace");
 const filter = require('filter-files');
 const findInFiles = require('find-in-files');
 const ncp = require('ncp').ncp;
+const Finder = require('fs-finder');
 
 // Clone a given repository into the `./tmp` folder.
 rimraf.sync(__dirname + '/templates')
-rimraf.sync(__dirname + '/final-templates')
-
-mkdirp('./final-templates')
+// rimraf.sync(__dirname + '/final-templates')
+// mkdirp('./final-templates')
 
 Git.Clone("https://github.com/googlesamples/android-architecture", "./templates")
   .then(function(repo) {
-    console.log("Cloning complete!");
-
-      checkOutAndCopy(repo,"todo-mvp", function() {
-        checkOutAndCopy(repo,"todo-mvp-loaders", function() {
-          console.log("Complete");
-        })
-      });
-      
-      // checkOutAndCopy(repo,"todo-databinding"),
-      // checkOutAndCopy(repo,"todo-mvp-clean"),
-      // checkOutAndCopy(repo,"todo-mvp-dagger"),
-      // checkOutAndCopy(repo,"todo-mvp-contentproviders")
+    // checkOutAndCopy(repo,"todo-mvp")
+    // checkOutAndCopy(repo,"todo-mvp-loaders");
+    // checkOutAndCopy(repo,"todo-mvp-clean")
+    // checkOutAndCopy(repo,"todo-mvp-dagger")
+    // checkOutAndCopy(repo,"todo-mvp-contentproviders")
+    checkOutAndCopy(repo,"todo-databinding");
   })
   .catch(function(err) {
     console.log(err);
   });
 
-function checkOutAndCopy(repo,name,callback) {
+function checkOutAndCopy(repo,name) {
   repo.getBranch('refs/remotes/origin/' + name)
     .then(function(reference) {
         console.log("Checking out branch "+ name);
         return repo.checkoutRef(reference);
     })
     .then(function() {
+      
       replace({
             regex: "com.example.android.architecture.blueprints.todoapp",
             replacement: "<%= appPackage %>",
             paths: ['./templates/todoapp'],
             recursive: true,
             silent: true,
+      });
+
+      mv('./templates/todoapp/.gitignore', './templates/todoapp/gitignore', function(err) {
+        console.log("Renamed root folder .gitignore")
+      });
+      
+      mv('./templates/todoapp/app/.gitignore', './templates/todoapp/app/gitignore', function(err) {
+        console.log("Renamed app folder .gitignore")
       });
 
       console.log("Copying files to ./final-templates/"+ name);
@@ -59,6 +63,5 @@ function checkOutAndCopy(repo,name,callback) {
        }
        console.log('Copying complete!');
       });
-    })
-    .then(callback);
+    });
   }
